b32314083b0f774ffcb71473678774f4678f8753
Omar Blas <blas.1234@hotmail.com>
2022-09-29 19:38:04 -0500

    Nueva clase de javascrip

---
+++ javascrip/curso.js
@@ -0,0 +1,36 @@
+/*var personas = {
+    "nombre":"blas",
+    "apellido":"flores",
+    edas:26,
+    correo:"blas.1234@hotmail.com"
+}*/
+
+class minombre{
+
+}
+
+
+/*let variable = {
+    nombre:"asus",
+    ram:"8gb"
+}
+
+let nombre=variable.nombre;
+let ram=variable.ram;
+
+let fraces = Mi computadora ${variable.nombre} tiene ${variable.ram}de ram ;
+
+var elmemo = new miClase("Guillermo","Quiñones");
+var el chino = new laPersona("Yahir","Sandobal");*/
+
+function laFuncion(){
+    return variabl1*2;
+}
+
+(variabl1) =>{return variabl1*2}
+
+variabl1 => {return variabl1*2}
+
+variabl1 =>variabl1*2;
+
+()=>variabl1*2;
