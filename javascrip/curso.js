3d92dda81e306013a9c68abc3230a1e4ac602c4f
Omar Blas <blas.1234@hotmail.com>
2022-10-03 04:33:03 -0500

    una disculpa por esta modificacion pero si me interesa mejorarlo un poco mas

--- javascrip/curso.js
+++ javascrip/curso.js
@@ -33,11 +33,46 @@ variabl1 => {return variabl1*2}
 
 variabl1 =>variabl1*2;
 
-()=>variabl1*2;*/
+()=>variabl1*2;
 
 var articulos =[
     {nombre: "Bici", costo: 3000},
     {nombre: "Tv", costo: 2500},
     {nombre: "Libro", costo: 320},
     {nombre: "Celular",costo}
-];
+];*/
+
+/*var nombre;     // introduccion basico
+
+nombre = prompt("ingresa tu nombre");
+edad = prompt("ingresa tu edad");
+
+document.write(nombre +" "+ edad);
+
+alert(nombre);*/
+
+// Tipos de datos
+    // Numerico Int o Float
+        var edad = 19.1234;
+    // String - Cadena de texto
+        var nombre = "omar";
+        var apellido = "blas";
+
+        document.write(nombre +" "+ apellido);
+    //Array - Arreglo (lo que hace este arreglo es cambiar los nombres en su posicion)
+        var compañeros = ["jack","ferM","Axel","Areli","Ing","Plata"];
+            compañeros[3] = "nohemi"; // aqui declaro la posicion a cambiar el nombre
+    
+        var amigos = ["D","E","F","A","B","C"];
+        var ordenados = amigos.sort();
+        document.write(ordenados);
+            //amigos.pop(); //elimina el ultimo campo del arreglo
+            //document.write( " Tiene:" + amigos.length + "Amigos");  // muestra el conteo total de todos los amigos 
+            //amigos[amigos.length] = "G";  // Agrega un amigo nuevo a un campo nuevo en el arreglo 
+            //amigos.push("G","H","I"); // esta funcion es lo mismo que el de arriba solo que puedes agregar varios de un solo golpe
+
+     //Object - Objeto
+    //Boolean - Boleano
+    //Undefined
+    //Null
+    //Nan
